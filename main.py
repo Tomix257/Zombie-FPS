5b4eff95018c5cf73ff9b85d3b01890bde3263bd
Tomáš <79867246+Tomix257@users.noreply.github.com>
2022-08-29 19:35:31 +0200

    big changes
    fixed few bugs i have found, changed few things for better frames, added healthbar and respawn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ursina import *
 from sun import SunLight
-from movement import FirstPersonController
+from player import Player
 
 # SETTINGS
 window.title = 'zombie game'
@@ -11,30 +11,27 @@ window.exit_button.disable()
 window.cog_button.disable()
 
 # MAP
-ground = Entity(model='plane', collider='box', scale=64, texture='grass', texture_scale=(4,4))
+ground = Entity(model='plane', collider='box', scale=64, texture='grass', texture_scale=(4,4),ignore=True)
 
 wall_1=Entity(model="cube", collider="cube", position=(-9, 0, 0), scale=(8, 5, 1), rotation=(0,0,0),
-    texture="brick", texture_scale=(5,5), color=color.rgb(255, 128, 0))
+    texture="brick", texture_scale=(5,5), color=color.rgb(255, 128, 0),ignore=True)
 wall_2 = duplicate(wall_1, z=5)
 wall_2=duplicate(wall_1, z=10)
 wall_2=duplicate(wall_1, z=15)
 wall_3=Entity(model="cube", collider="cube", position=(-15, 0, 10), scale=(1,5,20), rotation=(0,0,0),
-    texture="brick", texture_scale=(5,5), color=color.rgb(255, 128, 0))
-
-
+    texture="brick", texture_scale=(5,5), color=color.rgb(255, 128, 0),ignore=True)
 wall_4=Entity(model="cube", collider="cube", position=(9, 0, 0), scale=(8, 5, 1), rotation=(0,0,0),
-    texture="brick", texture_scale=(5,5), color=color.rgb(255, 128, 0))
+    texture="brick", texture_scale=(5,5), color=color.rgb(255, 128, 0),ignore=True)
 wall_5 = duplicate(wall_4, z=5)
 wall_5=duplicate(wall_4, z=10)
 wall_5=duplicate(wall_4, z=15)
 wall_6=Entity(model="cube", collider="cube", position=(15, 0, 10), scale=(1,5,20), rotation=(0,0,0),
-    texture="brick", texture_scale=(5,5), color=color.rgb(255, 128, 0))
-
+    texture="brick", texture_scale=(5,5), color=color.rgb(255, 128, 0),ignore=True)
 wall_7=Entity(model="cube", collider="cube", position=(-5, 0, -20), scale=(20, 5, 1), rotation=(0,0,0),
-    texture="brick", texture_scale=(5,5), color=color.rgb(255, 128, 0))
+    texture="brick", texture_scale=(5,5), color=color.rgb(255, 128, 0),ignore=True)
 
 # PLAYER
-player = FirstPersonController(model='cube',collider="cube", z=-10, color=color.light_gray, origin_y=-.5, speed=8)
+player = Player(model='cube',collider="box", z=-1, color=color.light_gray, origin_y=-.5)
 player.collider = BoxCollider(player, Vec3(0,1,0), Vec3(1,2,1))
 
 # GUN
@@ -52,8 +49,7 @@ def shoot():
     if not gun.on_cooldown:
         gun.on_cooldown = True
         gun.muzzle_flash.enabled=True
-        from ursina.prefabs.ursfx import ursfx
-        ursfx([(0.0, 0.0), (0.1, 0.9), (0.15, 0.75), (0.3, 0.14), (0.6, 0.0)], wave = 'assets/sounds/gun.wav', )
+        shooting =Audio('assets/sounds/gun.wav',loop = False)
         invoke(gun.muzzle_flash.disable, delay=.05)
         invoke(setattr, gun, 'on_cooldown', False, delay=.25)
         if mouse.hovered_entity and hasattr(mouse.hovered_entity, 'hp'):
@@ -62,7 +58,6 @@ def shoot():
 
 # HEALTHBAR
 from ursina.prefabs.health_bar import HealthBar
-
 
 class Enemy(Entity):
     def __init__(self, **kwargs):
@@ -79,14 +74,16 @@ class Enemy(Entity):
         self.health_bar.alpha = max(0, self.health_bar.alpha - time.dt)
 
         self.look_at_2d(player.position, 'y')
-        hit_info = raycast(self.world_position + Vec3(0,1,0), self.forward, 30, ignore=(self,))
+        hit_info = raycast(self.world_position + Vec3(0,1,0), self.forward, 50, ignore=(self,))
         if hit_info.entity == player:
             if dist > 2:
                 self.position += self.forward * time.dt * 5
-            if dist < 1.5:
+            if dist < 2:
                 player.damage()
 
     @property
+
+
     def hp(self):
         return self._hp
 
@@ -101,8 +98,7 @@ class Enemy(Entity):
         self.health_bar.alpha = 1
 
 # Enemy()
-enemies = [Enemy(x=x*-0.5,z=x*-0.5) for x in range(10)]
-    
+enemies = [Enemy(x=random.randint(3,10),z=random.randint(3,10)) for x in range(10)]
 
 # PAUSE
 pause_handler = Entity(ignore_paused=True)
@@ -115,9 +111,8 @@ def pause_handler_input(key):
 
 pause_handler.input = pause_handler_input   # Assign the input function to the pause handler.
 
-
 # TEXT
-Text('EARLY RELEASE!',color=color.yellow, origin=(4,2))
+Text('AMMO : INF/INF',color=color.white, origin=(-4, 18))
 # Lighting + shadows
 sun = SunLight(direction = (-0.7, -0.9, 0.5), resolution = 3072, player = player)
 ambient = AmbientLight(color = Vec4(0.5, 0.55, 0.66, 0) * 1.5)
